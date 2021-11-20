a99624e92ed2013350c2b79fe2db9bd1e166618f
Jesse <jdnely@gmail.com>
2021-11-19 16:13:05 -0800

    added tables, POST to hom in process

---
+++ db/migrate/20211119193517_create_games.rb
@@ -0,0 +1,11 @@
+class CreateGames < ActiveRecord::Migration[6.1]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.string :description
+      t.string :rules
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:'WaffleDino', password_digest: '12345')
+User.create(username:'Alumitti', password_digest: '54321')
